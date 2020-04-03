307adf7435f07fd1a9724e0b129b2047adfdfb53
aditya109 <adikid1996@gmail.com>
2020-04-03 15:51:15 +0530

    refactor code
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- main.py
+++
@@ -1,7 +0,0 @@
-import server.controller.driver as mainDriver
-
-
-if __name__ == "__main__" :
-    
-    app = mainDriver.BRS()
-    app.mainloop()
--- utils/helpers.py
+++
@@ -1,7 +0,0 @@
-import enum 
-
-class Font(enum.Enum):
-    LARGE_FONT = ("Verdana", 12)
-    MEDIUM_FONT = ("Verdana", 10)
-    SMALL_FONT = ("Verdana", 8)
-
