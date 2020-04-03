bd1c5d1e10105868a9848a89b8cbf258321cf618
aditya109 <adikid1996@gmail.com>
2020-04-03 15:48:11 +0530

    added versioning
    Signed-off-by: aditya109 <adikid1996@gmail.com>

---
+++ 1.1/windows/home.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# from ..helpers import Font 
+
+# class HomePage(tk.Frame) :
+#     def __init__(self, parent, controller) :
+#         tk.Frame.__init__(self, parent)
+#         label = tk.Label(self, text = "Home Page", font = Font.LARGE_FONT)
+#         label.pack(pady = 10, padx = 10)
+
+import os
+
+print(os.getcwd())
