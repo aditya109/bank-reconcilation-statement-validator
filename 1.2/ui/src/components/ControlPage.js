cd7317a4cd936ad35af391f50d943f832812424f
aditya109 <adikid1996@gmail.com>
2020-04-12 13:41:03 +0530

    starting control components
    Signed-off-by: aditya109 <adikid1996@gmail.com>

---
+++ 1.2/ui/src/components/ControlPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default class ControlPage extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+
+    render() {
+        return(
+            <div className = "control-page-main-container">
+                <div className = "control-page"></div>
+
+            </div>
+        )
+    }
+
+}
