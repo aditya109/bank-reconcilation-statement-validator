019ea4c2e973e1a44a1cad54916446b388f2c91d
Akanksha810 <32421005+Akanksha810@users.noreply.github.com>
2020-04-17 18:03:53 +0530

    Built a server that has three routes and displays the input given. (#36)
    Signed-off-by: Akanksha810 <shivi.s98@gmail.com>

---
+++ 1.2/server/main.py
@@ -0,0 +1,24 @@
+from flask import Flask,request
+import json 
+
+
+server = Flask(__name__)
+
+@server.route("/")
+
+def slash_path_handler():
+    return "api route / created ..."
+
+@server.route("/input", methods=['POST'])
+
+def input():
+    target_text = request.data
+    return target_text
+
+@server.route("/output", methods=['POST'])
+
+def output():
+    
+    return 
+
+server.run(port=5010, debug=True)
