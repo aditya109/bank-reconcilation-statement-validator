597d67c2fd272a8158775c8ac3f1eaaad13d1a82
aditya109 <adikid1996@gmail.com>
2020-07-02 13:06:42 +0530

    updating 1.3 ui
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- 1.3/frontend-ui/src/App.js
+++ 1.3/frontend-ui/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./presentational_components/Header";
+import Hero from "./presentational_components/Hero";
+import Upload from "./presentational_components/Upload";
+import Result from "./presentational_components/Result";
+import Footer from "./presentational_components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Hero/>
+      <Upload/>
+      <Result/>
+      <Footer/>
     </div>
   );
 }
