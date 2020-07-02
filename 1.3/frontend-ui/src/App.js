63a3f3113b1acc623d131bc4b2b3b8e42a3c28c6
aditya109 <adikid1996@gmail.com>
2020-07-02 10:27:51 +0530

    updating 1.3 ui
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- 1.3/frontend-ui/src/App.js
+++ 1.3/frontend-ui/src/App.js
@@ -1,30 +1,26 @@
-import React from "react";
-import {
-  BrowserRouter as Router, 
-  Route,
-} from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-
-import Home from "./pages/Home";
-
-import NavigationBar from './components/NavigationBar';
-import Test from './pages/Test';
-
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <NavigationBar/>
-        <Router>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/test" component={Test}/>
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
