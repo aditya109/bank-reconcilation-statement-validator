161ecb459005aea32cfdedae83150b4a6e77d8f0
Aditya Kumar <adikid1996@gmail.com>
2022-02-17 23:39:17 +0530

    removed react client and python server files;
    Signed-off-by: Aditya Kumar <adikid1996@gmail.com>

--- server/setup.py
+++
@@ -1,7 +0,0 @@
-# TODO write a proper setup please !!!
-from sample.server import trigger
-from config import provide_config
-
-if __name__ == "__main__":
-    config = provide_config()
-    trigger(configuration=config)
